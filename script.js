99a6edeba53dfe58c4ccc9728b5c14533e50ff9c
johnhur <johnjhur@gmail.com>
2015-05-14 14:51:55 -0700

    gradient checkerboard

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
-document.onload = function addElement () {
-	for (var i = 0; i < 81; i++) {
-		var colors= ['aqua', 'black', 'blue', 'fuchsia', 'gray', 'green', 
-		'lime', 'maroon', 'navy', 'olive', 'orange', 'purple', 'red', 
-		'silver', 'teal', 'white', 'yellow'];
-		if (i % 2 == 0) {
-			var newDiv = document.createElement("div");
-			newDiv.style.width = "11.1%";
-			newDiv.style.float = "left";
-			newDiv.style.paddingBottom = "11.1%";
-			newDiv.style.backgroundColor = colors[Math.floor((Math.random()*16)+1)];
+document.onload = function addElement(){
+		var r = 100;
+		var g = 40;
+		var b = 150;
+		var r1 = 5;
+		var g1 = 5;
+		var b1 = 5;	
+	for (var i = 0; i < 99; i++){
+		var col = "rgb(" + r + "," + g + "," + b + ")";
+		var col1 = "rgb(" + r1 + "," + g1 + "," + b1 + ")";
+		g += 2;
+		g1 += 2;
+		if (i % 2 === 0){
+			var newDiv = document.createElement('div');
+			newDiv.style.backgroundColor = col;
+  			newDiv.style.width = '11.1%';
+  			newDiv.style.float = 'left';
+  			newDiv.style.paddingBottom = '11.1%';
 			document.body.appendChild(newDiv);
-		} else {
-			var newDiv = document.createElement("div");
-			newDiv.style.width = "11.1%";
-			newDiv.style.float = "left";
-			newDiv.style.paddingBottom = "11.1%";
-			newDiv.style.backgroundColor = colors[Math.floor((Math.random()*16)+1)];
+		}else {
+			var newDiv = document.createElement('div');
+			newDiv.style.backgroundColor = col1;
+			newDiv.style.width = '11.1%';
+			newDiv.style.float = 'left';
+			newDiv.style.paddingBottom = '11.1%';
 			document.body.appendChild(newDiv);
-		}
+		}	
 	}
 }();
