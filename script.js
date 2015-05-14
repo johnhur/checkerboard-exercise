618626fa7f4a43b807042536bbd54e44d9dd2d52
johnhur <johnjhur@gmail.com>
2015-05-13 21:05:42 -0700

    randomcolor checkerboard

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 document.onload = function addElement () {
 	for (var i = 0; i < 81; i++) {
+		var colors= ['aqua', 'black', 'blue', 'fuchsia', 'gray', 'green', 
+		'lime', 'maroon', 'navy', 'olive', 'orange', 'purple', 'red', 
+		'silver', 'teal', 'white', 'yellow'];
 		if (i % 2 == 0) {
 			var newDiv = document.createElement("div");
 			newDiv.style.width = "11.1%";
 			newDiv.style.float = "left";
 			newDiv.style.paddingBottom = "11.1%";
-			newDiv.style.backgroundColor = 'red';
+			newDiv.style.backgroundColor = colors[Math.floor((Math.random()*16)+1)];
 			document.body.appendChild(newDiv);
 		} else {
 			var newDiv = document.createElement("div");
 			newDiv.style.width = "11.1%";
 			newDiv.style.float = "left";
 			newDiv.style.paddingBottom = "11.1%";
-			newDiv.style.backgroundColor = 'black';
+			newDiv.style.backgroundColor = colors[Math.floor((Math.random()*16)+1)];
 			document.body.appendChild(newDiv);
 		}
 	}
